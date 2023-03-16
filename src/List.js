3898b7c977369f41711c2fd507660f270325a9dd
Alan <alanones02@gmail.com>
2023-03-16 19:57:40 +0300

    first commit

---
+++ src/List.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+const List = (props) => {
+  const { people, removeItem } = props;
+  return (
+    <>
+      {people.map((person) => {
+        const { id, name, age, image } = person;
+        return (
+          <article key={id} className="person">
+            <img src={image} alt={name} />
+            <div>
+              <h4>{name}</h4>
+              <p>{age} years</p>
+              <span>
+                <button
+                  onClick={() => {
+                    removeItem(id);
+                  }}
+                >
+                  Remove {person.name}
+                </button>
+              </span>
+            </div>
+          </article>
+        );
+      })}
+    </>
+  );
+};
+
+export default List;
